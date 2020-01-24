9de63e0d872867ae7d72f4f3a49a4b63c33f1ed3
Yasin Ashraf <yasin.asharf@gmail.com>
2020-01-24 21:11:15 +0530

    add form entity

--- app/src/main/java/com/yasin/handzap/data/dao/FormsDao.kt
+++ app/src/main/java/com/yasin/handzap/data/dao/FormsDao.kt
@@ -8,4 +8,5 @@ import androidx.room.Dao
 @Dao
 interface FormsDao {
 
+
 }
---
+++ app/src/main/java/com/yasin/handzap/data/entity/Form.kt
@@ -0,0 +1,25 @@
+package com.yasin.handzap.data.entity
+
+import android.net.Uri
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+ * Created by Yasin on 24/1/20.
+ */
+@Entity
+data class Form(
+    @PrimaryKey
+    val id : String?,
+    val title : String?,
+    val date : String?,
+    val views : Int = 0,
+    val description : String?,
+    val budget : Double?,
+    val currency : String?,
+    val rate : String?,
+    val paymentMethod : String?,
+    val startDate : String?,
+    val jobTerm : String?,
+    val attachedFiles : List<Uri>
+)
